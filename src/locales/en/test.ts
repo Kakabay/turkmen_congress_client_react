1ee63d607182433cddfaf4a6f31bdf7c3e46599f
Batyr <awbatyr@gmail.com>
2025-06-03 13:22:31 +0500

    added info

---
+++ src/locales/en/test.ts
@@ -0,0 +1,20 @@
+// {
+//     "name": "Batyr Durdyyev",
+//     "speciality": "Business Consultant",
+
+//     "bio": [
+//       "Batyr Durdyyev has a Bachelor’s degree in Foreign Languages from Turkmen State University and a Postgraduate degree in Business and Management in Emerging Markets from the University of Reading, UK.",
+//       "Since 2005, he has served as Founder and CEO of TPS Advertising Agency, overseeing ventures including Tehnika Dunyasi (trading), Turkmen Forum (conferences), and Turkmen Expo (exhibitions), all of which have become key players in their sectors in Turkmenistan",
+//       "He is also active in social and professional circles, including the Chess Federation board, alumni networks, and business associations. He speaks Turkmen, Russian, English, and Turkish."
+//     ]
+//   },
+
+// {
+//     "name": "Батыр Дурдыев",
+//     "speciality": "Бизнес-консультант",
+//     "bio": [
+//       "Батыр Дурдыев получил степень бакалавра по специальности 'Иностранные языки' в Туркменском государственном университете и степень магистра в области бизнеса и управления на развивающихся рынках в “University of Reading” (Великобритания).",
+//       "С 2005 года он является основателем и генеральным директором рекламного агентства TPS, курируя такие проекты, как Tehnika Dunyasi (торговля), Turkmen Forum (конференции) и Turkmen Expo (выставки), которые стали ведущими игроками в своих отраслях в Туркменистане.",
+//       "Он также активно участвует в общественной и профессиональной жизни — входит в состав правления Федерации шахмат, является членом ассоциаций выпускников и бизнес-сообществ. Владеет туркменским, русским, английским и турецким языками."
+//     ]
+//   },
