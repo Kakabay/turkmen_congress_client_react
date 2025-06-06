56dc63cfaf13974c647610d7251456aea79d1057
Batyr <awbatyr@gmail.com>
2025-06-06 15:26:02 +0500

    build

--- src/components/hero/HeroSection.tsx
+++ src/components/hero/HeroSection.tsx
@@ -11,15 +11,23 @@ const HeroSection = () => {
         id="hero"
       >
         {/* <div className="absolute top-0 left-0 size-full bg-gradient-to-b backdrop-blur-[8%] from-[#0D3767]/0 to-[#0D3767]/20 -z-10" /> */}
-        <div className="absolute top-0 left-0 size-full">
-          <img src="/hero/1.jpg" className="size-full object-cover" />
+        <div className="absolute top-0 left-0 size-full brightness-[15%]">
+          <video
+            muted
+            autoPlay
+            loop
+            src="https://qacis.turkmenexpo.com/app/storage/app/media/video/1%20Conference%202025_1.mp4"
+            className="size-full object-cover"
+          />
         </div>
 
         <Container>
-          <div className="h-screen w-full flex flex-col justify-end pb-2 gap-2">
-            <h1 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[1000px] w-full font-80-medium">
-              {t("hero.title")}
-            </h1>
+          <div className="h-screen w-full flex flex-col justify-end pb-2 gap-2 relative z-30">
+            <h1
+              className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[1000px] w-full font-80-medium"
+              dangerouslySetInnerHTML={{ __html: String(t("hero.title")) }}
+            />
+
             <div className="flex w-full bottom-6 justify-between items-end">
               <span className="font-14-regular">{t("hero.venue")}</span>
               <span className="font-18-regular max-w-[518px] w-full">
